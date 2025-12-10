dda727a0d26159a9dda8b8928c0c001e48938426
Dawidk887 <dawid.kurkarewicz@onet.pl>
2025-12-10 11:07:36 +0000

    move files

---
+++ src/main/java/com/example/demoDockerCompose/User.java
@@ -0,0 +1,40 @@
+package com.example.demoDockerCompose;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
